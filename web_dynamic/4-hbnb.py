279669927d0c69e3b64e43bb152a9cb5d4136fb1
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-05 17:50:19 +0100

    Update 4-hbnb.py

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -4,7 +4,7 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
+from models.place import Place   
 from os import environ
 from flask import Flask, render_template
 import uuid
