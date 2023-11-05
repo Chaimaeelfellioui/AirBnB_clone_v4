7124ce796d41aa50bb5d9e015a4bfe7854603e73
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-05 18:03:47 +0100

    Update 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,7 +8,7 @@ function init () {
     if ($(this).is(':checked')) {
       amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
     } else if ($(this).is(':not(:checked)')) {
-      delete amenityObj[$(this).attr('data-name')];
+      delete amenityObj[$(this).attr('data-name')];  
     }
     const names = Object.keys(amenityObj);
     $('.amenities h4').text(names.sort().join(', '));
