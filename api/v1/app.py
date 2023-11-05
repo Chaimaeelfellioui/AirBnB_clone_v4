a19c923c3436379af04d5e18fa5fabcf86437e89
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-05 18:15:13 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
+from flask_cors import CORS  
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
